64a0358ded29e779ccbdf8bdd194f7ba0b17b8d9
Bartosz Muszarski <bartosz.muszarski@arm.com>
2024-01-23 14:02:57 +0100

    Add missing flush-inside-measurement-range option to android
    Change-Id: I3964be478fa6d0cb11e90b26d7b10346fd6837a5

--- android/scripts/gfxrecon.py
+++ android/scripts/gfxrecon.py
@@ -125,6 +125,7 @@ def CreateReplayParser():
     parser.add_argument('-m', '--memory-translation', metavar='MODE', choices=['none', 'remap', 'realign', 'rebind'], help='Enable memory translation for replay on GPUs with memory types that are not compatible with the capture GPU\'s memory types.  Available modes are: none, remap, realign, rebind (forwarded to replay tool)')
     parser.add_argument('--swapchain', metavar='MODE', choices=['virtual', 'captured', 'offscreen'], help='Choose a swapchain mode to replay. Available modes are: virtual, captured, offscreen (forwarded to replay tool)')
     parser.add_argument('--use-captured-swapchain-indices', action='store_true', default=False, help='Same as "--swapchain captured". Ignored if the "--swapchain" option is used.')
+    parser.add_argument('--flush-inside-measurement-range', action='store_true', default=False, help='If this is specified the replayer will flush and wait for all current GPU work to finish at the end of each frame inside the measurement range')
     parser.add_argument('file', nargs='?', help='File on device to play (forwarded to replay tool)')
 
     return parser
@@ -259,6 +260,9 @@ def MakeExtrasString(args):
 
     if args.dsf:
         arg_list.append('--dsf')
+
+    if args.flush_inside_measurement_range:
+        arg_list.append('--flush-inside-measurement-range')
 
     if args.file:
         arg_list.append(args.file)
