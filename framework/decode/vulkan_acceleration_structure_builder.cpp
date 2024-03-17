e2e3b51a3d1b0f2152a4c6553ee342b0eebe9ede
Bartosz Muszarski <bartosz.muszarski@arm.com>
2024-03-17 21:50:27 +0100

    Handle portable shader group handles
    With this change shader group handles are replaced before QueueSubmit
    based on information from tracked vkGetRayTracingShaderGroupHandlesKHR
    and vkCmdTraceRays.
    
    This implementation assumes shader group handle size and alignment
    is the same on the capture and replay device, will hit assert otherwise.
    This condition is true on all tested RTU's - v0,1 and 2
    
    Change-Id: I34a3c8f02b870cce45c9fc4a9b7bdd43576035a7

--- framework/decode/vulkan_acceleration_structure_builder.cpp
+++ framework/decode/vulkan_acceleration_structure_builder.cpp
@@ -30,12 +30,18 @@ GFXRECON_BEGIN_NAMESPACE(gfxrecon)
 GFXRECON_BEGIN_NAMESPACE(decode)
 
 VulkanAccelerationStructureBuilder::VulkanAccelerationStructureBuilder(
-    Functions                               functions,
-    VkDevice                                device,
-    VulkanResourceAllocator*                allocator,
-    const VkPhysicalDeviceMemoryProperties& memory_properties) :
+    Functions                                        functions,
+    VkDevice                                         device,
+    VulkanResourceAllocator*                         allocator,
+    const VkPhysicalDeviceMemoryProperties&          memory_properties,
+    VkPhysicalDeviceRayTracingPipelinePropertiesKHR  ray_tracing_pipeline_properties,
+    VkPhysicalDeviceAccelerationStructureFeaturesKHR acceleration_structure_features) :
     functions_(functions),
-    device_(device), allocator_(allocator), physical_device_memory_properties_(memory_properties)
+    device_(device), allocator_(allocator), physical_device_memory_properties_(memory_properties),
+    ray_tracing_pipeline_properties_(ray_tracing_pipeline_properties),
+    acceleration_structure_features_(acceleration_structure_features),
+    raytracing_pipeline_properties_(ray_tracing_pipeline_properties.shaderGroupHandleSize,
+                                    ray_tracing_pipeline_properties.shaderGroupHandleAlignment)
 {}
 
 VulkanAccelerationStructureBuilder::~VulkanAccelerationStructureBuilder()
@@ -1064,17 +1070,112 @@ void VulkanAccelerationStructureBuilder::OnQueueSubmit(uint32_t submitCount, con
             ++it;
         }
     }
+
+    // Update shader group handles if the submission contains vkCmdTraceRays command
+    for (int i = 0; i < submitCount; ++i)
+    {
+        auto submission = pSubmits[i];
+        for (int cmd_buffer_index = 0; cmd_buffer_index < submission.commandBufferCount; ++cmd_buffer_index)
+        {
+            auto submitted_buffer = submission.pCommandBuffers[cmd_buffer_index];
+
+            // Every command buffer containing CmdTraceRays has been recorded into trace_rays_ map along with the
+            // shader binding table data. These SBTs will contain shader group handles that need an update
+            for (auto& sbt_data : trace_rays_[submitted_buffer])
+            {
+                UpdateShaderBindingTable(sbt_data.raygen);
+                UpdateShaderBindingTable(sbt_data.hit);
+                UpdateShaderBindingTable(sbt_data.miss);
+                UpdateShaderBindingTable(sbt_data.callable);
+            }
+            trace_rays_.erase(submitted_buffer);
+        }
+    }
 }
 
-void VulkanAccelerationStructureBuilder::OnCmdTraceRaysKHR(VkStridedDeviceAddressRegionKHR* pRaygenShaderBindingTable,
+void VulkanAccelerationStructureBuilder::UpdateShaderBindingTable(const VkStridedDeviceAddressRegionKHR& sbt_entry)
+{
+    // early exit - address/size can be 0, nothing to update
+    if (!sbt_entry.size || !sbt_entry.deviceAddress)
+    {
+        return;
+    }
+    uint8_t*           mapped_sbt_buffer;
+    const BufferEntry* sbt_buffer = GetBufferByRuntimeDeviceAddress(sbt_entry.deviceAddress);
+    allocator_->MapResourceMemoryDirect(sbt_entry.size, 0, (void**)&mapped_sbt_buffer, sbt_buffer->allocator_data_);
+
+    // sbt device address may be offsetted, account for that in
+    const size_t sbt_offset = sbt_entry.deviceAddress - sbt_buffer->new_address_;
+
+    // points to the sbt within mapped buffer
+    uint8_t* sbt_start_address = mapped_sbt_buffer + sbt_offset;
+
+    // goes over all the known shader group handles tracked in the trace
+    // compares them with the values in the mapped buffer at provided offset
+    // and replaces if there's a match
+    auto update_entry = [&](VkDeviceSize handle_offset) {
+        uint8_t* handle_address = sbt_start_address + handle_offset;
+        for (const auto& entry : shader_group_handle_entries_)
+        {
+            // Assume shaderGroupHandleSize and shaderGroupHandleAlignment are the same - verified on rtuv0,rtuv1,rtuv2
+            // TODO: shader group size/alignment might change on different platforms
+            assert(entry.original_data_.size() == entry.runtime_data_.size() ==
+                   raytracing_pipeline_properties_.shader_group_handle_size_aligned);
+            if (0 == memcmp(entry.original_data_.data(), handle_address, entry.original_data_.size()))
+            {
+                memcpy(handle_address, entry.runtime_data_.data(), entry.original_data_.size());
+            }
+        }
+    };
+
+    // stride may be 0 if there's just a single value in the sbt
+    if (sbt_entry.stride > 0)
+    {
+        for (VkDeviceSize handle_offset = 0; handle_offset < sbt_entry.size; handle_offset += sbt_entry.stride)
+        {
+            update_entry(handle_offset);
+        }
+    }
+    else
+    {
+        update_entry(0);
+    }
+
+    allocator_->UnmapResourceMemoryDirect(sbt_buffer->allocator_data_);
+}
+
+void VulkanAccelerationStructureBuilder::OnCmdTraceRaysKHR(VkCommandBuffer                  command_buffer,
+                                                           VkStridedDeviceAddressRegionKHR* pRaygenShaderBindingTable,
                                                            VkStridedDeviceAddressRegionKHR* pMissShaderBindingTable,
                                                            VkStridedDeviceAddressRegionKHR* pHitShaderBindingTable,
                                                            VkStridedDeviceAddressRegionKHR* pCallableShaderBindingTable)
 {
+    // SBT's device addresses can change, update them based on GetBufferDeviceAddress calls recorded earlier
     UpdateBufferDeviceAddress(pRaygenShaderBindingTable->deviceAddress);
     UpdateBufferDeviceAddress(pMissShaderBindingTable->deviceAddress);
     UpdateBufferDeviceAddress(pHitShaderBindingTable->deviceAddress);
     UpdateBufferDeviceAddress(pCallableShaderBindingTable->deviceAddress);
+
+    // Shader group handles stored in SBT's will require update as well, this should be done before QueueSubmit
+    // Store SBT data for later replacement
+    CmdTraceRaysEntry new_entry{
+        *pRaygenShaderBindingTable, *pMissShaderBindingTable, *pHitShaderBindingTable, *pCallableShaderBindingTable
+    };
+    trace_rays_[command_buffer].push_back(new_entry);
+}
+
+void VulkanAccelerationStructureBuilder::RegisterShaderGroupHandleEntry(uint32_t group_count,
+                                                                        size_t   data_size,
+                                                                        uint8_t* original_data,
+                                                                        uint8_t* runtime_data)
+{
+    const auto single_entry_size = data_size / group_count;
+    for (uint32_t group_index = 0; group_index < group_count; ++group_index)
+    {
+        shader_group_handle_entries_.emplace_back(original_data + (single_entry_size * group_index),
+                                                  runtime_data + (single_entry_size * group_index),
+                                                  single_entry_size);
+    }
 }
 
 void VulkanAccelerationStructureBuilder::PostQueuePresent()
--- framework/decode/vulkan_replay_consumer_base.cpp
+++ framework/decode/vulkan_replay_consumer_base.cpp
@@ -2893,12 +2893,33 @@ VulkanReplayConsumerBase::OverrideCreateDevice(VkResult            original_resu
                     .cmd_pipeline_barrier           = device_table->CmdPipelineBarrier,
                     .create_query_pool              = device_table->CreateQueryPool
                 };
+
+                auto table = GetInstanceTable(physical_device);
+
+                VkPhysicalDeviceRayTracingPipelinePropertiesKHR ray_tracing_pipeline_properties{};
+                ray_tracing_pipeline_properties.sType =
+                    VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_TRACING_PIPELINE_PROPERTIES_KHR;
+                VkPhysicalDeviceProperties2 device_properties{};
+                device_properties.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2;
+                device_properties.pNext = &ray_tracing_pipeline_properties;
+                table->GetPhysicalDeviceProperties2(physical_device, &device_properties);
+
+                VkPhysicalDeviceAccelerationStructureFeaturesKHR acceleration_structure_features{};
+                acceleration_structure_features.sType =
+                    VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_ACCELERATION_STRUCTURE_FEATURES_KHR;
+                VkPhysicalDeviceFeatures2 device_features{};
+                device_features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+                device_features.pNext = &acceleration_structure_features;
+                table->GetPhysicalDeviceFeatures2(physical_device, &device_features);
+
                 acceleration_structure_builders_[*pDevice->GetPointer()] =
                     std::make_unique<VulkanAccelerationStructureBuilder>(
                         as_builder_functions,
                         *replay_device,
                         allocator,
-                        *physical_device_info->replay_device_info->memory_properties);
+                        *physical_device_info->replay_device_info->memory_properties,
+                        ray_tracing_pipeline_properties,
+                        acceleration_structure_features);
             }
         }
 
@@ -3676,7 +3697,7 @@ VkResult VulkanReplayConsumerBase::OverrideQueueSubmit(PFN_vkQueueSubmit func,
 
     if ((options_.sync_queue_submissions) && (result == VK_SUCCESS))
     {
-        GetDeviceTable(queue_info->handle)->QueueWaitIdle(queue_info->handle);
+        GFXRECON_ASSERT(VK_SUCCESS == GetDeviceTable(queue_info->handle)->QueueWaitIdle(queue_info->handle));
     }
 
     if (screenshot_handler_ != nullptr)
@@ -7462,7 +7483,17 @@ VkResult VulkanReplayConsumerBase::OverrideCreateRayTracingPipelinesKHR(
                                                          &pPipelines->GetPointer()[createInfoCount]);
     }
 
-    if (device_info->property_feature_info.feature_rayTracingPipelineShaderGroupHandleCaptureReplay)
+    if (!device_info->allocator->SupportsOpaqueDeviceAddresses())
+    {
+        result = device_table->CreateRayTracingPipelinesKHR(device,
+                                                            in_deferredOperation,
+                                                            overridePipelineCache,
+                                                            createInfoCount,
+                                                            in_pCreateInfos,
+                                                            in_pAllocator,
+                                                            out_pPipelines);
+    }
+    else if (device_info->property_feature_info.feature_rayTracingPipelineShaderGroupHandleCaptureReplay)
     {
         // Modify pipeline create infos with capture replay flag and data.
         std::vector<VkRayTracingPipelineCreateInfoKHR>                 modified_create_infos;
@@ -7790,7 +7821,8 @@ VulkanReplayConsumerBase::OverrideGetRayTracingShaderGroupHandlesKHR(PFN_vkGetRa
     assert((device_info != nullptr) && (pipeline_info != nullptr) && (pData != nullptr) &&
            (pData->GetOutputPointer() != nullptr));
 
-    if (!device_info->property_feature_info.feature_rayTracingPipelineShaderGroupHandleCaptureReplay)
+    if (!device_info->property_feature_info.feature_rayTracingPipelineShaderGroupHandleCaptureReplay &&
+        device_info->allocator->SupportsOpaqueDeviceAddresses())
     {
         GFXRECON_LOG_WARNING_ONCE(
             "The captured application used vkGetRayTracingShaderGroupHandlesKHR, which may require the "
@@ -7802,7 +7834,14 @@ VulkanReplayConsumerBase::OverrideGetRayTracingShaderGroupHandlesKHR(PFN_vkGetRa
     VkPipeline pipeline    = pipeline_info->handle;
     uint8_t*   output_data = pData->GetOutputPointer();
 
-    return func(device, pipeline, firstGroup, groupCount, dataSize, output_data);
+    VkResult result = func(device, pipeline, firstGroup, groupCount, dataSize, output_data);
+
+    if (!device_info->allocator->SupportsOpaqueDeviceAddresses())
+    {
+        acceleration_structure_builders_[device_info->capture_id]->RegisterShaderGroupHandleEntry(
+            groupCount, dataSize, pData->GetPointer(), pData->GetOutputPointer());
+    }
+    return result;
 }
 
 VkResult VulkanReplayConsumerBase::OverrideGetAndroidHardwareBufferPropertiesANDROID(
@@ -9011,6 +9050,7 @@ void VulkanReplayConsumerBase::OverrideCmdTraceRaysKHR(
     if (!allocator->SupportsOpaqueDeviceAddresses())
     {
         acceleration_structure_builders_[in_commandBuffer->parent_id]->OnCmdTraceRaysKHR(
+            in_commandBuffer->handle,
             pRaygenShaderBindingTable->GetPointer(),
             pMissShaderBindingTable->GetPointer(),
             pHitShaderBindingTable->GetPointer(),
