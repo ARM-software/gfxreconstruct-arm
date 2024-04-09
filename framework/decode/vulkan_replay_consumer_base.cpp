3244683b41e9ee5ee8df2fdefaf9e22f7f56bf69
Bartosz Muszarski <bartosz.muszarski@arm.com>
2024-04-09 15:38:56 +0200

    Add device fault handling for sync option
    Also change display format of addresses in device faults to hex.
    Currently if QueueWaitIdle injected by --sync option fails we get following log
    
    Assertion failed: 'VK_SUCCESS == GetDeviceTable(queue_info->handle)->
    QueueWaitIdle(queue_info->handle)'
    
    With this change the log will display more useful details like call index and device fault info:
    
    Address type: VK_DEVICE_FAULT_ADDRESS_TYPE_READ_INVALID_EXT
    Reported address: 0x8002061a7000
    Address precision: 4096
    Vendor description: ...
    Vendor fault code: 193
    Vendor fault data: 0x1210002c1
    API call at index: 39938 thread: 2 (SYNC) vkQueueWaitIdle returned error value
    VK_ERROR_DEVICE_LOST that does not match the result from the capture file:
    VK_SUCCESS. Replay cannot continue.
    
    Also - minor bugfix in gfxrecon.py for android
    
    Change-Id: I0460b6370fe89b985562bd1b140e0d44f21c3c54

--- framework/decode/vulkan_replay_consumer_base.cpp
+++ framework/decode/vulkan_replay_consumer_base.cpp
@@ -1208,7 +1208,7 @@ void VulkanReplayConsumerBase::CheckResult(const char*                 func_name
         {
             GFXRECON_LOG_ERROR("Address type: %s",
                                util::ToString<VkDeviceFaultAddressTypeEXT>(address_info.addressType).c_str());
-            GFXRECON_LOG_ERROR("Reported address: %" PRIu64, address_info.reportedAddress);
+            GFXRECON_LOG_ERROR("Reported address: 0x%" PRIx64, address_info.reportedAddress);
             GFXRECON_LOG_ERROR("Address precision: %" PRIu64, address_info.addressPrecision);
         }
 
@@ -1216,7 +1216,7 @@ void VulkanReplayConsumerBase::CheckResult(const char*                 func_name
         {
             GFXRECON_LOG_ERROR("Vendor description: %s", vendor_info.description);
             GFXRECON_LOG_ERROR("Vendor fault code: %" PRIu64, vendor_info.vendorFaultCode);
-            GFXRECON_LOG_ERROR("Vendor fault data: %" PRIu64, vendor_info.vendorFaultData);
+            GFXRECON_LOG_ERROR("Vendor fault data: 0x%" PRIx64, vendor_info.vendorFaultData);
         }
         if (device_fault_vendor_data_supported && !device_fault_info.vendor_binary_data_.empty())
         {
@@ -3724,10 +3724,6 @@ VkResult VulkanReplayConsumerBase::OverrideQueueSubmit(PFN_vkQueueSubmit func,
         }
     }
 
-    if ((options_.sync_queue_submissions) && (result == VK_SUCCESS))
-    {
-        GFXRECON_ASSERT(VK_SUCCESS == GetDeviceTable(queue_info->handle)->QueueWaitIdle(queue_info->handle));
-    }
 
     if (screenshot_handler_ != nullptr)
     {
