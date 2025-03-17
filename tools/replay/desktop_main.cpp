69ff814b0396106f4a0b3d8f03a1cc2abe856d85
Marius Pelegrin <marius.pelegrin@arm.com>
2025-03-17 10:53:53 +0100

    Remove waiting for input key at the end of Windows replay
    This wait leads to automation timeouts when used with the
    Windows Emulation Stack (and is not useful internally
    anyway)
    
    Change-Id: Ibe4cf901d9a10153ec0e184aec9c2dbe04db5667

--- tools/replay/desktop_main.cpp
+++ tools/replay/desktop_main.cpp
@@ -70,17 +70,18 @@ void WaitForExit()
 {
     DWORD process_list[2];
     DWORD result = GetConsoleProcessList(process_list, ARRAYSIZE(process_list));
-
     // If the process list contains a single entry, we assume that the console was created when the gfxrecon-replay.exe
     // process started, and will be destroyed when it exits.  In this case, we will wait on user input before exiting
     // and closing the console window to give the user a chance to read any console output.
     if (result <= 1)
     {
-        GFXRECON_WRITE_CONSOLE("\nPress any key to close this window . . .");
-        while (!_kbhit())
-        {
-            Sleep(250);
-        }
+        // Waiting stucks some automations and is mainly useless for us
+
+        // GFXRECON_WRITE_CONSOLE("\nPress any key to close this window . . .");
+        // while (!_kbhit())
+        // {
+        //     Sleep(250);
+        // }
     }
 }
 #else
